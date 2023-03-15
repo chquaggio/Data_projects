b4cf146deded0eff57da850a04d1f7c665a8fdf4
ChristianQuaggioAlkemy <christian.quaggio@alkemy.com>
2023-03-15 15:10:31 +0100

    gradio update

--- gradio/demo_final_interface.py
+++ gradio/demo_final_interface.py
@@ -13,39 +13,32 @@ inputs = {}
 def save_img(npa):
     im = PIL.Image.fromarray(npa.astype('uint8'), 'RGB')
     im.save("my_image_input.png")
-#inputs = {
-#	'prompt': xx,
-#	'negative_prompt': xx,
-#	'input_image': open("path/to/image", "rb"),
-#	'prompt_strength': xx
-#	'num_outputs': 1,
-#	'num_inference_steps': xx,
-#	'guidance_scale': xx,
-#	'scheduler': 'DPMSolverMultistep'
-#}
 
 #function to read inputs, manipulate them and deliver outputs
-def reader(pos_prompts, neg_prompts, prompt_strength, num_inference_steps, guidance_scale, image):
+def reader(pos_prompts, neg_prompts, num_outputs, prompt_strength, num_inference_steps, guidance_scale, image):
     save_img(image)
     inputs['prompt'] = pos_prompts
     inputs['negative_prompt'] = neg_prompts
     inputs['input_image'] = open('/home/dev/Data_projects/gradio/my_image_input.png', 'rb') 
-    inputs['num_outputs'] = 1
+    inputs['num_outputs'] = num_outputs
     inputs['num_inference_steps'] = num_inference_steps
     inputs['prompt_strength'] = prompt_strength
     inputs['guidance_scale'] = guidance_scale
     inputs['scheduler'] = 'DPMSolverMultistep'
 
     output = version.predict(**inputs)
-    response = requests.get(output[0])
+    gallery = []
+    for i in range(num_outputs):
+        response = requests.get(output[i])
+        gallery.append(PIL.Image.open(BytesIO(response.content)))
 
-    return PIL.Image.open(BytesIO(response.content))
+    return gallery
 
 #definitionof the interface
 demo = gr.Interface(
     fn=reader,
-    inputs=[gr.Textbox(lines=2, placeholder="Enter your positive prompts here (comma separated)"), gr.Textbox(lines=2, placeholder="Enter your negative prompts here (comma separated)"), gr.Slider(0, 1, step=0.05), gr.Slider(1, 500), gr.Slider(1, 20),"image"],
-    outputs=["image"],
+    inputs=[gr.Textbox(lines=2, placeholder="Enter your positive prompts here (comma separated)"), gr.Textbox(lines=2, placeholder="Enter your negative prompts here (comma separated)"), gr.Slider(1, 8, step=1), gr.Slider(0, 1, step=0.05), gr.Slider(1, 500), gr.Slider(1, 20),"image"],
+    outputs=[gr.Gallery(label="Generated images", show_label=True).style(grid=[2], height="auto")],
     )
 demo.launch(server_name="0.0.0.0")
 
